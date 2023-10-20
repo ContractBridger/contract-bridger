184eab6e7efc0a47a1ab62f7409d8ff84e5bce30
olaoluwa057 <fatolu057olaoluwa@gmail.com>
2023-10-20 07:05:39 +0100

    feat: added mock endpoints

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,24 @@
 import express from 'express';
 const routes = express.Router();
 
-routes.get('/', (_, res) => res.json({ success: true, message: 'Server running' }));
+// Dummy data for demonstration purposes.
+const smartContracts = [
+  { name: 'Contract1', address: '0x123456789', sourceCode: '...your source code...' },
+  { name: 'Contract2', address: '0x987654321', sourceCode: '...your source code...' },
+];
+
+// Endpoint to pull smart contracts
+routes.get('/smart-contracts', (_, res) => {
+  res.json({ success: true, smartContracts });
+});
+
+// Endpoint to compile bytecode (dummy implementation)
+routes.post('/compile-bytecode', (req, res) => {
+  // Typically we can send the source code to a compiler and return the compiled bytecode.
+  const sourceCode = req.body.sourceCode;
+  const bytecode = '0xcompiledbytecode'; // I'd replace with tha  actual compilation logic.
+
+  res.json({ success: true, bytecode });
+});
 
 export default routes;
