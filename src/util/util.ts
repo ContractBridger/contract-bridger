f4455475346bbf02305b1f7428e79b89c9e62d85
Jeremiah Noah <newcreationjerry96@gmail.com>
2023-10-21 00:32:14 +0100

    feat: updated pullContractDetailFunction to use chainId

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,9 +8,29 @@ const fs = require('fs');
 require("dotenv").config();
 
 
-export async function pullContractDetailFromSourceChain(contractAddress: string, etherscanAPIKey: string): Promise< {contractName: string, compilerVersion: any, optimizationUsed: any, runs: any,sourceCode: any}>{
-  const res = await axios.get('https://api.etherscan.io/api?module=contract&action=getsourcecode&address='+contractAddress+'&apikey='+etherscanAPIKey);
- 
+export async function pullContractDetailFromSourceChain(contractAddress: string, chainId: number): Promise< {contractName: string, compilerVersion: any, optimizationUsed: any, runs: any,sourceCode: any}>{
+    let chainExplorerString: string = ''
+    let apikey: any = ''
+    
+    //mainnet
+    if(chainId == 1 ){
+        chainExplorerString = 'api.etherscan.io'
+        apikey = process.env.ETHERSCAN_API_KEY
+
+        //bsc
+    }else if(chainId == 56){
+        chainExplorerString = 'api.bscscan.com'
+        apikey = process.env.BSC_API_KEY
+
+
+    }else if(chainId == 137){ //polygon
+        chainExplorerString = 'api.polygonscan.com'
+        apikey = process.env.POLYGON_API_KEY
+    }
+
+
+    const res = await axios.get('https://'+chainExplorerString+'/api?module=contract&action=getsourcecode&address='+contractAddress+'&apikey='+apikey);
+    console.log(res, "res in func", apikey)
   const dataResponse = res.data.result[0];
   const sourceCode : any = dataResponse.SourceCode
   const contractName : string = dataResponse.ContractName
@@ -66,7 +86,4 @@ export async function compileContract(contractName: string, sourceCode:any ): Pr
     return byteCodeAfterCompilation
     
 }
-
-
-
 
